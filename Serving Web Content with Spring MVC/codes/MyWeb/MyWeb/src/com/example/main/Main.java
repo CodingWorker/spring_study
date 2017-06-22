5f6ce2fa126da03c33d04523e4fbfe6a42f31d03
daiyan <daiyan@htyunwang.com>
2017-06-22 17:20:37 +0800

    add

---
+++ Serving Web Content with Spring MVC/codes/MyWeb/MyWeb/src/com/example/main/Main.java
@@ -0,0 +1,11 @@
+package com.example.main;
+
+/**
+ * Created by daiya on 2017/6/22.
+ */
+public class Main {
+    public static void main(String[] args){
+        Greeter greeter=new Greeter();
+        System.out.println(greeter.sayHello());
+    }
+}
